a40fd539e55bf1351ccfb4e3d120211c19e561ca
dueduardoful <dueduardoful@gmail.com>
2020-01-09 18:39:33 -0300

    Na Classe Client foram declaradas variaveis, feitos os construtores e getters e setters

--- src/entities/Client.java
+++ src/entities/Client.java
@@ -1,5 +1,38 @@
 package entities;
+import java.util.Date;
 
 public class Client {
-
+	//1. declaração de variáveis
+	private String name;
+	private String email;
+	private Date birthDate;
+	//2. construtor padrão
+	public Client() {	
+	}
+	//3. construtor com argumentos
+	public Client(String name, String email, Date birthDate) {
+		this.name = name;
+		this.email = email;
+		this.birthDate = birthDate;
+	}
+	//4. Getters e Setters
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	
 }
