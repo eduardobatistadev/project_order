ded1acd1a2d2053ed3eaa49944923e94858b807f
dueduardoful <dueduardoful@gmail.com>
2020-01-12 19:13:41 -0300

    Feito declaração de variaveis, construtores, getters e setters

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -1,5 +1,33 @@
 package entities;
 
 public class Product {
-
+	//1. declaração de variáveis
+	private String name;
+	private Double price;
+	
+	//2. Construtor padrão
+	
+	public Product() {
+	}
+	//3. construtor com argumentos
+	public Product(String name, Double price) {
+		this.name = name;
+		this.price = price;
+	}
+	// 4. Getters e Setters
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+	
+	
 }
