2a15266b7e41c5c7d5450748b3d90d405f8abf59
dueduardoful <dueduardoful@gmail.com>
2020-01-12 21:02:13 -0300

    Feito: Declaração de variaveis, construtores, getters e setters.

--- src/entities/OrderItem.java
+++ src/entities/OrderItem.java
@@ -1,5 +1,29 @@
 package entities;
 
 public class OrderItem {
+	//1. declaração de variaveis
+	private Integer quantity;
+	private Double price;
+	//2. construtor padrão
+	public OrderItem() {
+	}
+	//3. construtor com argumentos
+	public OrderItem(Integer quantity, Double price) {
+		this.quantity = quantity;
+		this.price = price;
+	}
+	// 4. getters e setters
+	public Integer getQuantity() {
+		return quantity;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
 
 }
