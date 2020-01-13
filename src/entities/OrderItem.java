b756bce4b66268d87ac18ab506dc0cacd0036321
dueduardoful <dueduardoful@gmail.com>
2020-01-13 09:26:48 -0300

    feito complementações

--- src/entities/OrderItem.java
+++ src/entities/OrderItem.java
@@ -4,26 +4,50 @@ public class OrderItem {
 	//1. declaração de variaveis
 	private Integer quantity;
 	private Double price;
-	//2. construtor padrão
-	public OrderItem() {
-	}
+	
+	private Product product;
 	//3. construtor com argumentos
 	public OrderItem(Integer quantity, Double price) {
 		this.quantity = quantity;
 		this.price = price;
+		this.product = product;
 	}
 	// 4. getters e setters
 	public Integer getQuantity() {
 		return quantity;
 	}
-	public Double getPrice() {
-		return price;
-	}
 	public void setQuantity(Integer quantity) {
 		this.quantity = quantity;
 	}
+	public Double getPrice() {
+		return price;
+	}
+
 	public void setPrice(Double price) {
 		this.price = price;
+	}
+	
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public double subTotal() {
+		return price * quantity;
+	}
+	
+	@Override
+	public String toString() {
+		return product.getName() 
+				+ ", $" 
+				+ String.format("%.2f", price) 
+				+ ", Quantity: " 
+				+ quantity + 
+				", Subtotal: $" 
+				+ String.format("%.2f", subTotal());
 	}
 
 }
